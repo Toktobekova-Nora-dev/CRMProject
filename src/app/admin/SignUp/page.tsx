b76fffced13fe70931eeae9349a61cee75a28a89
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-05-23 18:16:35 +0600

    signin

--- src/app/admin/SignUp/page.tsx
+++ src/app/admin/SignUp/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>Sign Up</div>;
+  return (
+    <div>
+      <h1>Войти в систему</h1>
+    </div>
+  );
 };
 
 export default page;
--- src/components/AdminDashboard/signinPage/SigninPage.tsx
+++ src/components/AdminDashboard/signinPage/SigninPage.tsx
@@ -1,21 +1,42 @@
 import React from "react";
-import scss from "./SignonPage.module.scss";
+import { FiMail, FiLock } from "react-icons/fi";
+import scss from "./SigninPage.module.scss";
+import Link from "next/link";
+
 const SigninPage = () => {
   return (
-    <div>
-      <div className={scss.box}>
+    <div className={scss.content}>
+      <div className={scss.left}>
         <h1>Добро пожаловать!</h1>
         <p>
-          Войдите в систему, чтобы управлять записями,
-          <br /> пациентами и процессами клиники легко и удобно.
+          Войдите в систему, чтобы управлять записями, <br />
+          пациентами и процессами клиники легко и удобно.
         </p>
-        <button>ВОЙТИ</button>
+        <Link href={"/admin/signUp"}>
+          <button className={scss.leftButton}>ВОЙТИ</button>
+        </Link>
       </div>
-      <div>
-        <form action="">
-          <input type="text" />
-          <input type="text" />
-          <input type="submit" />
+      <div className={scss.right}>
+        <h2>Войти в систему</h2>
+        <form>
+          <label>
+            Email
+            <div className={scss.inputGroup}>
+              <FiMail />
+              <input type="email" placeholder="Введите свою почту" />
+            </div>
+          </label>
+          <label>
+            Пароль
+            <div className={scss.inputGroup}>
+              <FiLock />
+              <input type="password" placeholder="Введите свой пароль" />
+            </div>
+          </label>
+
+          <button type="submit" className={scss.rightButton}>
+            Войти
+          </button>
         </form>
       </div>
     </div>
