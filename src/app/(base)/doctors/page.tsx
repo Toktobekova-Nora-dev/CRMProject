0dff12604a601329da438aa1c573ca5e4a00dffc
Eohan <elamanju5@gmail.com>
2025-05-25 16:32:14 +0600

    b/1

---
+++ src/app/(base)/doctors/page.tsx
@@ -0,0 +1,12 @@
+import ListOfDoctors from "@/components/pages/ListOfDoctors/ListOfDoctors";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <ListOfDoctors />
+    </div>
+  );
+};
+
+export default page;
