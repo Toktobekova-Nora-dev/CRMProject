c06614f166b064b8431f36ec864c3cebbff713f0
bakdoolot <nurisulajmanov.com>
2025-05-29 15:18:57 +0600

    edit-doctor

--- src/components/client/pages/editDoctor/EditDoctor.tsx
+++ src/components/client/pages/editDoctor/EditDoctor.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React from "react";
+import styles from "./EditDoctor.module.scss";
+import justLogo from "../../../../../public/img/Frame 1171275422.png";
 
 const EditDoctor = () => {
   return (
-    <div>
-      <div className="container">
-        <div className="editDoctor">
-          <h1>Edit Doctor</h1>
+    <section className={styles.editDoctor}>
+      <div className={styles.container}>
+        <div className={styles.editDoctorContent}>
+          <img src={justLogo} alt="logo" />
+          <label htmlFor="doctorName">Имя врача</label>
+          <input id="doctorName" type="text" defaultValue="Канатбек Аскеров" />
+
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" defaultValue="kanatbek@example.com" />
+
+          <label htmlFor="contacts">Контакты</label>
+          <input id="contacts" type="text" defaultValue="+996 700 000 000" />
+
+          <label htmlFor="department">Отделение</label>
+          <select id="department">
+            <option value="cardiology">Кардиология</option>
+            <option value="neurology">Неврология</option>
+            <option value="pediatrics">Педиатрия</option>
+          </select>
+
+          <label htmlFor="position">Должность</label>
+          <select id="position">
+            <option value="senior">Старший врач</option>
+            <option value="junior">Младший врач</option>
+            <option value="intern">Интерн</option>
+          </select>
+
+          <label htmlFor="bonus">Введите бонус в процентах</label>
+          <input id="bonus" type="number" defaultValue={10} />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
