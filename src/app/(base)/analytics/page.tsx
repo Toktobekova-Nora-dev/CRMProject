4254d81c5be3725b51b6278a8e1ea7712ba93427
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-06-13 12:15:02 +0600

    a/2

--- src/app/(base)/analytics/page.tsx
+++ src/app/(base)/analytics/page.tsx
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-import AnalitcsPage from "@/components/client/pages/analitcs/AnalitcsPage";
-import React from "react";
-const page = () => {
-  return (
-    <div>
-      <AnalitcsPage />
-    </div>
-  );
-};
-=======
 import Analytics from "@/components/client/pages/analytics/Analytics";
 import React from "react";
 const page = () => <Analytics />;
->>>>>>> f5a781447cc7d6dd0cb4ddca52219dba5a3dc2a4
 
 export default page;
