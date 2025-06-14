e3c3aced14bd53bdf08d7b0d0ac56e7219e4b1a6
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-06-14 12:05:03 +0600

    a

--- src/app/(base)/analytics/report/page.tsx
+++ src/app/(base)/analytics/report/page.tsx
@@ -1,10 +1,10 @@
-import Reports from '@/components/client/pages/analytics/reports/Reports';
-import React from 'react';
+import Reports from "@/components/client/pages/analytics/reports/Reports";
+import React from "react";
 
 const page = () => {
   return (
     <div>
-      <Reports/>
+      <Reports />
     </div>
   );
 };
